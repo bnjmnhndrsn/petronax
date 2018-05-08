ad87c58aa299113cab01ea022528df35c69040c5
Ben <benjamin.c.henderson@gmail.com>
2018-05-07 23:57:01 -0400

    get client built

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,106 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import moment from 'moment';
+
 import './App.css';
 
 class App extends Component {
-  constructor(){
-    super();
-    this.state = {
-      data: null
-    };
-  }
-  
-  componentDidMount(){
-    fetch('/api/wikipedia?date=1950-01-05')
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      this.setState({data: data[0]})
-    });
-  }
-  
-  render() {
-    return (
-      <div>{ JSON.stringify(this.state.data)  }</div>
-    );
-  }
+    constructor(){
+        super();
+        this.state = {
+            photo: null,
+            isFetching: false,
+            year: (Math.floor((Math.random() * 100)) + 1900).toString()
+        };
+        
+        this.onChange = this.onChange.bind(this);
+        this.onBlur = this.onBlur.bind(this);
+    }
+    
+    componentDidMount(){
+        this.fetchPhoto();
+    }
+    
+    fetchPhoto(){
+        if (!this._hasValidYear()) {
+            return;
+        }
+        
+        this.setState({
+            isFetching: true
+        });
+        
+        const dateString = moment().year(this.state.year).format('YYYY-MM-DD');
+        axios.get(`/api/wikipedia?date=${dateString}`)
+        .then(({data}) => {
+            let newPhoto = null;
+            data = data.filter(datum => /(jpg|jpeg|png)$/.test(datum.image_url));
+            
+            if (data.length) {
+                newPhoto = data[Math.floor(Math.random() * data.length)]
+            }
+            this.setState({photo: newPhoto, isFetching: false});
+        });
+    }
+    
+    _hasValidYear(){
+        const parsed = Number(this.state.year);
+        return parsed > 1800 && parsed <= moment().year();
+    }
+    
+    onChange(ev){
+        this.setState({year: ev.target.value, showError: false}, () => this.fetchPhoto());
+    }
+    
+    onBlur(){
+        if (!this._hasValidYear()) {
+            this.setState({showError: true});
+        }    
+    }
+    
+    _renderPhoto(){
+        if (this.state.photo) {
+            return (
+                <div className="photo">
+                    <div className="title">
+                        {this.state.photo.title}
+                    </div>
+                    <div>
+                        <a href={this.state.photo.description_url} target="_blank">
+                            <img alt={this.state.photo.title} src={this.state.photo.image_url}/>
+                        </a>
+                    </div>
+                </div>
+            );
+        } else {
+            return <div>No Photo</div>
+        }
+    }
+
+    render() {
+        return (
+            <div className="app">
+                <div className="header">
+                    <div>
+                    From this day in <input value={this.state.year} onChange={this.onChange} onBlur={this.onBlur} />
+                    </div>
+                    <div className="warning">
+                    {
+                        (!this._hasValidYear() && this.state.showError) ?
+                        'Please enter a year between 1800 and now' :
+                        ' '
+                    }
+                    </div>
+                </div>
+
+                {
+                    this.state.isFetching ?
+                    <div>Loading Photo...</div> :
+                    this._renderPhoto()
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
