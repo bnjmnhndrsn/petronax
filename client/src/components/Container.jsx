3afc7b10b9a4545e94b2e1b09b1e8bc097f3c197
Ben Henderson <ben@nestio.com>
2018-06-09 23:21:27 -0400

    maybe almost get it working?

--- client/src/components/Container.jsx
+++ client/src/components/Container.jsx
@@ -23,7 +23,8 @@ const DATE_FORMAT = 'YYYY-MM-DD';
 class App extends Component {
     componentDidMount(){
         this.props.setDate(
-            moment().year((Math.floor((Math.random() * 15)) + 2000).toString()).format(DATE_FORMAT)
+            //moment().year((Math.floor((Math.random() * 15)) + 2000).toString()).format(DATE_FORMAT)
+            '2018-05-30'
         );
     }
 
--- client/src/components/Photos.jsx
+++ client/src/components/Photos.jsx
@@ -80,29 +80,33 @@ export default class Photos extends Component {
 
         const dates = times(renderedDates, (i) => {
             const date = moment(currentDate).subtract(dateOffset, 'day').add(i, 'day')
-            return {
-                index: date.diff(moment(DATE_MIN, DATE_FORMAT), 'days'),
-                date: date.format(DATE_FORMAT)
-            }
+            return date.format(DATE_FORMAT);
         });
 
         const containerSize = this.props.windowWidth;
+        const renderedSize = renderedDates * this.props.photoWidth;
         const totalSize = totalPossibleDates * this.props.photoWidth;
         const safeTotalSize = Math.min(getMaxElementSize(), totalPossibleDates * this.props.photoWidth);
-        // const offset = this.state.scrollPos;
-        // const offsetPercentage = totalSize <= containerSize ? 0 : offset / (totalSize - containerSize);
-        // const offsetAdjustment = Math.round(offsetPercentage * (safeTotalSize - totalSize));
-        //
+        const initialIndex = moment(dates[0], DATE_FORMAT).diff(moment(DATE_MIN, DATE_FORMAT), 'days');
+
+        const offset = this.state.scrollPos;
+        const offsetPercentage = safeTotalSize <= renderedSize ? 0 : offset / (safeTotalSize - containerSize);
+        const offsetAdjustment = Math.round(offsetPercentage * (safeTotalSize - totalSize));
+
         return (
             <div style={{overflow: 'visible', width: 0}}>
                 <div className="photos-wrapper" style={{width: `${this.props.windowWidth}px`}} ref={this.bindEl} onScroll={this.onScroll}>
                     <div className="photos-container" style={{width: `${safeTotalSize}px` }}>
                         {
-                            dates.map(obj => (
-                                <div key={obj.date} style={{position: 'absolute', top: '0', bottom: '0', left: `${(obj.index * this.props.photoWidth) *  ((safeTotalSize - this.props.windowWidth) / (totalSize - this.props.windowWidth))}px`, width: this.props.photoWidth}}>
-                                    <Photo date={obj.date} photoWidth={this.props.photoWidth} />
-                                </div>
-                            ))
+                            dates.map((date, i) => {
+                                const firstPosition = (initialIndex * this.props.photoWidth);
+                                const left = firstPosition + (i * this.props.photoWidth) + offsetAdjustment;
+                                return (
+                                    <div key={date} style={{position: 'absolute', top: '0', bottom: '0', left: `${left}px`, width: this.props.photoWidth}}>
+                                        <Photo date={date} photoWidth={this.props.photoWidth} />
+                                    </div>
+                                );
+                            })
                         }
                     </div>
                 </div>
