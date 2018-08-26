5496097d80f0fd655d97071e245152c8a38809a8
Ben <benjamin.c.henderson@gmail.com>
2018-08-26 13:53:26 -0400

    start including description key in API

--- utils/wikipedia.js
+++ utils/wikipedia.js
@@ -1,5 +1,6 @@
 var axios = require('axios');
 var lodash = require('lodash');
+var striptags = require('striptags');
 
 var BASE_URL = 'https://commons.wikimedia.org/w/api.php';
 
@@ -16,9 +17,9 @@ var WikipediaAPI = {
             gsrwhat: 'text',
             gsrlimit: limit,
             prop: 'imageinfo',
-            iiprop: 'url',
+            iiprop: 'url|extmetadata',
             gsroffset: offset,
-            iiurlwidth: 1200
+            iiurlwidth: 1200,
         };
 
         return axios.get(BASE_URL, { params: params })
@@ -30,13 +31,19 @@ var WikipediaAPI = {
                 return [];
             } else {
                 return lodash.map(response.data.query.pages, function(page){
+                    var description = null;
+                    var metadata = page.imageinfo[0].extmetadata;
+                    if (metadata.ImageDescription && metadata.ImageDescription.value) {
+                        description = striptags(metadata.ImageDescription.value);
+                    }
                     return {
                         page_id: page.pageid,
                         title: page.title,
                         index: page.index,
                         image_url: page.imageinfo[0].url,
                         description_url: page.imageinfo[0].descriptionurl,
-                        scaled_url: page.imageinfo[0].thumburl
+                        scaled_url: page.imageinfo[0].thumburl,
+                        description: description
                     }
                 });
             }
