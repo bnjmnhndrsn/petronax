0acb6d4687baf4da7160db8de98e3c3b37e327f3
Ben Henderson <ben@nestio.com>
2019-05-06 23:15:01 -0400

    begin trying react-window

--- client/src/components/CalendarDates.jsx
+++ client/src/components/CalendarDates.jsx
@@ -4,24 +4,31 @@ import moment from 'moment';
 import VirtualizedDragManager from './VirtualizedDragManager';
 import { DATE_FORMAT, DATE_MIN, DATE_MAX } from '../constants';
 import CalendarDate from './CalendarDate';
+import { FixedSizeGrid as Grid } from 'react-window';
 
 export default class CalendarDates extends Component {
     constructor(){
         super();
         this.renderItem = this.renderItem.bind(this);
         this.onIndexChange = this.onIndexChange.bind(this);
+        this.grid = React.createRef();
     }
+    
+    // componentDidMount(){
+    //     const currentIndex = moment(this.props.date, DATE_FORMAT).diff(moment(DATE_MIN, DATE_FORMAT), 'days');
+    //     this.grid.current && this.grid.current.scrollToItem({index})
+    // }
 
-    renderItem({index, style}){
+    renderItem({columnIndex, style}){
         return (
-            <div className="photo-wrapper" key={index} style={style}>
-                <CalendarDate date={moment(DATE_MIN, DATE_FORMAT).add(index, 'days').format(DATE_FORMAT)} photoWidth={this.props.photoWidth} />
+            <div className="photo-wrapper" key={columnIndex} style={style}>
+                <CalendarDate date={moment(DATE_MIN, DATE_FORMAT).add(columnIndex, 'days').format(DATE_FORMAT)} photoWidth={this.props.photoWidth} />
             </div>
         )
     }
 
-    onIndexChange(index){
-        this.props.setDate(moment(DATE_MIN, DATE_FORMAT).add(index, 'days').format(DATE_FORMAT));
+    onIndexChange({visibleColumnStartIndex}){
+        this.props.setDate(moment(DATE_MIN, DATE_FORMAT).add(visibleColumnStartIndex, 'days').format(DATE_FORMAT));
     }
 
     render(){
@@ -29,21 +36,35 @@ export default class CalendarDates extends Component {
         const itemWidth = this.props.photoWidth;
         const totalPossibleDates = moment(DATE_MAX, DATE_FORMAT).diff(moment(DATE_MIN, DATE_FORMAT), 'days')  + 1;
         const currentIndex = moment(this.props.date, DATE_FORMAT).diff(moment(DATE_MIN, DATE_FORMAT), 'days');
-
+        
         return (
             <div className="calendar-dates">
-                <VirtualizedDragManager
-                    bufferSize={2}
-                    scrollDirection="horizontal"
-                    currentIndex={currentIndex}
-                    containerSize={containerWidth}
-                    itemSize={itemWidth}
-                    totalItems={totalPossibleDates}
-                    renderItem={this.renderItem}
-                    onIndexChange={this.onIndexChange}
-                />
+                <Grid
+                    columnCount={totalPossibleDates}
+                    columnWidth={itemWidth}
+                    rowCount={1}
+                    height={500}
+                    rowHeight={500}
+                    width={containerWidth}
+                    onItemsRendered={this.onIndexChange}
+                    ref={this.grid}
+                    initialScrollLeft={currentIndex * itemWidth}
+                >
+                    { this.renderItem }
+                </Grid>
             </div>
 
         )
     }
 }
+
+// <VirtualizedDragManager
+//     bufferSize={2}
+//     scrollDirection="horizontal"
+//     currentIndex={currentIndex}
+//     containerSize={containerWidth}
+//     itemSize={itemWidth}
+//     totalItems={totalPossibleDates}
+//     renderItem={this.renderItem}
+//     onIndexChange={this.onIndexChange}
+// />
